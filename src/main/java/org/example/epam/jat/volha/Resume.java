9589671b9e4b65672761a8ca2b20d17aaf2b1301
volhakr <kresiko@gmail.com>
2023-03-10 11:38:55 +0200

    Initial version

--- src/main/java/org/example/epam/jat/volha/Resume.java
+++ src/main/java/org/example/epam/jat/volha/Resume.java
@@ -1,2 +1,46 @@
-package org.example.epam.jat.volha;public class Resume {
+package org.example.epam.jat.volha;
+
+public class Resume {
+  private int candidateId;
+  private String candidateNameAndSurname;
+  private String technology;
+  private Integer yearsOfExperience;
+  public int getCandidateId() {
+    return candidateId;
+  }
+
+  public void setCandidateId(int candidateId) {
+    this.candidateId = candidateId;
+  }
+
+  public String getCandidateNameAndSurname() {
+    return candidateNameAndSurname;
+  }
+
+  public void setCandidateNameAndSurname(String candidateNameAndSurname) {
+    this.candidateNameAndSurname = candidateNameAndSurname;
+  }
+
+  public String getTechnology() {
+    return technology;
+  }
+
+  public void setTechnology(String technology) {
+    this.technology = technology;
+  }
+
+  public Integer getYearsOfExperience() {
+    return yearsOfExperience;
+  }
+
+  public void setYearsOfExperience(Integer yearsOfExperience) {
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+
+  public void searchResume() {
+    //logic to search resume from some repo goes here
+
+  }
 }
+
